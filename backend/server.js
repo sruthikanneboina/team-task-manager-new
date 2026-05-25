c90de0bd39e4deccd8128ea3e0bba114c0394ff3
Sruthi Yadav <sruthi9810@gmail.com>
2026-05-25 16:51:58 +0530

    added register api

--- backend/server.js
+++ backend/server.js
@@ -21,13 +21,53 @@ app.get("/", (req, res) => {
   res.send("Backend Running")
 })
 
+app.post("/register", async (req, res) => {
+  try {
+
+    const { name, email, password } = req.body
+
+    const existingUser = await User.findOne({ email })
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists"
+      })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword
+    })
+
+    await newUser.save()
+
+    res.json({
+      message: "User registered successfully"
+    })
+
+  } catch (err) {
+
+    console.log(err)
+
+    res.status(500).json({
+      message: "Server Error"
+    })
+
+  }
+})
+
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
+
   console.log("MongoDB Connected")
 
   app.listen(process.env.PORT || 5000, () => {
     console.log("Server running")
   })
+
 })
 .catch((err) => {
   console.log(err)
